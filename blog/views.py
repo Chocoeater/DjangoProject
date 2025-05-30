2a7d3ce3af55feffdb88ed75618d59e4a327a5e7
Chocoeater <lonelypain98@gmail.com>
2025-05-30 20:52:53 +0300

    update BlogRecordDetail: add count of view and send mail

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
@@ -12,10 +14,34 @@ class BlogRecordListView(ListView):
     context_object_name = 'records'
     paginate_by = 3
 
+    def get_queryset(self):
+        self.queryset = super().get_queryset()
+        new_queryset = [x for x in self.queryset if x.publication is True]
+        return new_queryset
+
+
 class BlogRecordDetailView(DetailView):
     model = BlogRecord
     template_name = 'record_detail.html'
     context_object_name = 'record'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_of_views += 1
+        self.object.save()
+
+        if self.object.number_of_views == 100:
+            send_mail(
+                subject='Поздравляем!',
+                message=f'Статья {self.object.title} просмотрели 100 раз!',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=['kourdackov@yandex.ru'],
+                fail_silently=False
+            )
+
+        return self.object
+
+
 
 class BlogRecordCreateView(CreateView):
     model = BlogRecord
